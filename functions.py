8bcab0c10b57219476b3ea3e3d6701a82d92b332
Shivaram Kumar V.R <shivaramkumar375@gmail.com>
2024-10-05 22:44:53 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,14 @@
+def add():
+    print("Addition :",a+b)
+def sub():
+    print("Subtraction :",a-b)
+def mul():
+    print("Multiplication :",a*b)
+def div():
+    print("Division :",a/b)
+a=int(input("a?"))
+b=int(input("b?"))
+add()
+sub()
+mul()
+div()
